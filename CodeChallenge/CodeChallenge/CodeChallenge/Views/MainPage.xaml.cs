580e4ee8dc79a55cb3e329ced2d0b53ffedbad2d
TarcisioVitor <tarcisiosouzabr@gmail.com>
2017-09-05 20:53:59 -0300

    ProjectStart

---
+++ CodeChallenge/CodeChallenge/CodeChallenge/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CodeChallenge.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
