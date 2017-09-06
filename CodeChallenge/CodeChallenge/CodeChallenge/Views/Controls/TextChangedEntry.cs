7a6306ea108d3932d8f8ccd8591dcf1c9ed07376
TarcisioVitor <tarcisiosouzabr@gmail.com>
2017-09-06 00:49:37 -0300

    Entry TextChanged

---
+++ CodeChallenge/CodeChallenge/CodeChallenge/Views/Controls/TextChangedEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CodeChallenge.Views.Controls
+{
+
+    public class TextChangedEntry : Entry , IDisposable
+    {
+        public ICommand TextChangedCommand
+        {
+            get { return (Command)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(TextChangedCommand), typeof(Command), typeof(TextChangedEntry), defaultBindingMode: BindingMode.OneWay, defaultValue: default(Command));
+
+        public TextChangedEntry()
+        {
+            this.TextChanged += TextChangedEntry_TextChanged;
+        }
+
+        private void TextChangedEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.TextChangedCommand.Execute(e.NewTextValue);
+        }
+
+        public void Dispose()
+        {
+            this.TextChanged -= TextChangedEntry_TextChanged;
+        }
+    }
+}
